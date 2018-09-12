0ae809decfc936f05a858c0cc1523449799a176c
srinivasan <sri9132@gmail.com>
2018-09-12 01:52:13 -0700

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={	
+	'ApiUrl':"http://kdboys.chiarro.com/driver/api",
+	'DialogDefaultTitle':"Kdb Driver App",	
+	'PushProjectID':"",	
+	'APIHasKey':""
+};
