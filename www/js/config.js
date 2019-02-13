f39faca75d97891dfe02c21d7bfd752d41e92c84
srikutty19 <sri9132@gmail.com>
2019-02-13 21:06:45 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
 /*Driver App Configuration*/
 
 var krms_driver_config ={	
-	'ApiUrl':"http://kdboys.chiarro.com/driver/api",
-	'DialogDefaultTitle':"Kdb Driver App",	
+	'ApiUrl':"http://foodieschoice.tk/driver/api",
+	'DialogDefaultTitle':"foodies Driver App",	
 	'PushProjectID':"465251440510",	
 	'APIHasKey':""
 };
