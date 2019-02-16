4f58fdf3fdc62c9377226e4a75d38125167edf3d
srikutty19 <sri9132@gmail.com>
2019-02-16 10:22:04 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -3,6 +3,6 @@
 var krms_driver_config ={	
 	'ApiUrl':"http://foodieschoice.tk/driver/api",
 	'DialogDefaultTitle':"foodies Driver App",	
-	'PushProjectID':"465251440510",	
-	'APIHasKey':"mykey"
+	'PushProjectID':"",	
+	'APIHasKey':""
 };
